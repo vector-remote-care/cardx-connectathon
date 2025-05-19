e852fba2efb14922aabd7cea3478c79467530c50
Ash Woods <ash@whatup.email>
2025-05-19 07:15:14 -0700

    Concurrency and getDeviceResources

--- src/fetch.js
+++ src/fetch.js
@@ -1,7 +1,10 @@
 const axios = require('axios');
+const throat = require('throat');
+
+const axiosThroatGet = throat(1, axios.get);
 
 module.exports.getObservationCiedConnectivityResources = async () => {
-  const data = await axios.get('http://hapi.fhir.org/baseR5/Observation?code=observation-cied-connectivity', {
+  const data = await axiosThroatGet('http://hapi.fhir.org/baseR5/Observation?code=observation-cied-connectivity', {
     headers: {
       'Accept': 'application/fhir+xml;q=1.0, application/fhir+json;q=1.0, application/xml+fhir;q=0.9, application/json+fhir;q=0.9',
       'User-Agent': 'Vector-Remote-Care/Node.js'
@@ -13,4 +16,16 @@ module.exports.getObservationCiedConnectivityResources = async () => {
   }
 
   return data.data.entry.map((entry) => entry.resource);
+};
+
+module.exports.getDeviceResources = async (deviceId) => {
+  const data = await axiosThroatGet(`http://hapi.fhir.org/baseR5/Device?_id=${deviceId}`, {
+    headers: {
+      'Accept': 'application/fhir+xml;q=1.0, application/fhir+json;q=1.0, application/xml+fhir;q=0.9, application/json+fhir;q=0.9',
+      'User-Agent': 'Vector-Remote-Care/Node.js'
+    }
+  });
+  
+  return data.data.entry?.[0].resource;
+
 };
