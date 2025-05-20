a63d8dbce3732cf6263faa8946400df9f1d3b019
Ash Woods <ash@whatup.email>
2025-05-20 06:24:41 -0700

    Patient fetch

--- src/fetch.js
+++ src/fetch.js
@@ -27,5 +27,15 @@ module.exports.getDeviceResources = async (deviceId) => {
   });
   
   return data.data.entry?.[0].resource;
+};
 
+module.exports.getPatientResources = async (patientId) => {
+  const data = await axiosThroatGet(`http://hapi.fhir.org/baseR5/Patient?_id=${patientId}`, {
+    headers: {
+      'Accept': 'application/fhir+xml;q=1.0, application/fhir+json;q=1.0, application/xml+fhir;q=0.9, application/json+fhir;q=0.9',
+      'User-Agent': 'Vector-Remote-Care/Node.js'
+    }
+  });
+  
+  return data.data.entry?.[0].resource;
 };
